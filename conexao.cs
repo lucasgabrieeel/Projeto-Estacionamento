053f290ba247a70a2279c876a9cd052b64ef1546
lucasgabrieeel <lug.silva167@gmail.com>
2025-07-21 11:30:17 -0300

    Atualização de nomes e codigos errados

---
+++ conexao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using MySql.Data.MySqlClient;
+
+namespace estacionamento_2
+{
+
+    class Conexao
+    {
+        private MySqlConnection login;
+
+        public void Conectar()
+        {
+            // Lê o arquivo config.json
+            string jsonString = File.ReadAllText("config.json");
+            Config config = JsonSerializer.Deserialize<Config>(jsonString);
+
+            // Monta a string de conexão
+            string connStr = $"server={config.Servidor};uid={config.Usuario};pwd={config.Senha};database={config.Banco};";
+
+            login = new MySqlConnection(connStr);
+            login.Open();
+        }
+
+        public void Desconectar()
+        {
+            if (login != null && login.State == System.Data.ConnectionState.Open)
+                login.Close();
+        }
+    }
+}
+
